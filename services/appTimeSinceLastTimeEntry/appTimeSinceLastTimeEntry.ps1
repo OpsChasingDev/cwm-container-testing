e109c43a0d81b1e100ff1c1c5482a7a8d159d532
OpsChasingDev <robert@stapleton-family.net>
2026-01-15 20:27:43 -0500

    added check and logging for $dataPath

--- services/appTimeSinceLastTimeEntry/appTimeSinceLastTimeEntry.ps1
+++ services/appTimeSinceLastTimeEntry/appTimeSinceLastTimeEntry.ps1
@@ -19,6 +19,13 @@ New-CWMLog -Type "Info" -Message "Data path: $dataPath"
 
 # Main loop
 while ($true) {
+    # Shows app is running
     New-CWMLog -Type "Info" -Message "Service is running normally"
+    # Check if data path is accessible
+    if (Test-Path $dataPath) {
+        New-CWMLog -Type "Info" -Message "Data path is accessible: $dataPath"
+    } else {
+        New-CWMLog -Type "Error" -Message "Data path is not accessible: $dataPath"
+    }
     Start-Sleep -Seconds 5
 }
